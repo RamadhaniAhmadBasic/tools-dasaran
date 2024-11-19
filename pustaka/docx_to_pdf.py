6c08b336728e030cf5b97d969bdd3cbcfabd8e5b
RamadhaniAhmadBasic <ramadhani.ahmad.basic@gmail.com>
2024-11-19 14:11:59 +0700

    Delete pustaka directory

--- pustaka/docx_to_pdf.py
+++
@@ -1,8 +0,0 @@
-from docx2pdf import convert
-import pustaka.tabel_info
-
-def f():
-	docx_file_path = input("Masukkan direktori tempat file DOCX berada: ")
-	pdf_file_path = input("Masukkan nama file PDF output: ")
-	convert(docx_file_path, pdf_file_path)
-	pustaka.tabel_info.f("SUCCESS", f"Konversi {docx_file_path} ke {pdf_file_path}")
