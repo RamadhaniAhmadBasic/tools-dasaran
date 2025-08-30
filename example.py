5c71ac7322ceed83da7cdaf67326773324faed0a
RamadhaniAhmadBasic <ramadhani.ahmad.basic@gmail.com>
2025-08-30 09:36:54 +0700

    Update example.py

--- example.py
+++ example.py
@@ -5,34 +5,18 @@ import textwrap
 
 # Define Variable
 
-fg_black_c   = "\033[30m"
 fg_red_c     = "\033[31m" # ERROE color
 fg_green_c   = "\033[32m" # SUCCESS color
 fg_yellow_c  = "\033[33m" # WARNING color
-fg_blue_c    = "\033[34m"
-fg_magenta_c = "\033[35m"
 fg_cyan_c    = "\033[36m" # INFO color
-fg_grey_c    = "\033[37m"
 
-bg_black_c   = "\033[40m"
-bg_red_c     = "\033[41m"
-bg_green_c   = "\033[42m"
-bg_yellow_c  = "\033[43m"
-bg_blue_c    = "\033[44m"
-bg_magenta_c = "\033[45m"
-bg_cyan_c    = "\033[46m"
-bg_grey_c    = "\033[47m"
+bg_red_c     = "\033[41m" # ERROR color
+bg_green_c   = "\033[42m" # SUCCESS color
+bg_yellow_c  = "\033[43m" # WARNING color
+bg_cyan_c    = "\033[46m" # INFO color
 
-reset_c      = "\033[0m" # RESET color
-bold_c       = "\033[1m"
-dim_c        = "\033[2m"
-italic_c     = "\033[3m"
-underline_c  = "\033[4m"
-sblink_c     = "\033[5m"
-fblink_c     = "\033[6m"
-invert_c     = "\033[7m"
-hidden_c     = "\033[8m"
-strike_c     = "\033[9m"
+reset_c      = "\033[0m"  # RESET color
+invert_c     = "\033[7m"  # INVERT color
 
 # Define Function
 def title_p(title_value):
